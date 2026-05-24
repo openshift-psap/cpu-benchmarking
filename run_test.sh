b32b1a139ec74772b92284e3ebcbc58bc5a8c73f
Naveen Miriyalu <nmiriyal@redhat.com>
2026-05-24 14:57:01 -0400

    Add configs

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,5 @@
- python3 cpu_vllm_bench.py --config downstream.json \
- --dashboard-csv prefill-decode-study-Qwen3-4B-8B-sweep.csv \
+ python3 cpu_vllm_bench.py --config  upstream-qwen3-4b-v0.21.0.json --config   upstream-qwen3-8b-v0.21.0.json  \
+ --dashboard-csv prefill-decode-study-Qwen3-4B-8B-sweep-SPR-upstreamonly-v0.21.0.csv \
   --dashboard-accelerator "SPR" \
   --mlflow \
-  --output-base SPR/Prefill-Decode-Sweep
+  --output-base SPR/Prefill-Decode-Sweep-UpstreamOnly-v0.21.0-SingleSocket
