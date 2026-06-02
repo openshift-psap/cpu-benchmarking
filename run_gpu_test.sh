927125ebfe5c4c9310af451d6cc4ad8970f1a1f4
Naveen Miriyalu <nmiriyal@redhat.com>
2026-06-01 20:11:20 -0400

    Add gpu configs

--- run_gpu_test.sh
+++ run_gpu_test.sh
@@ -1,5 +1,5 @@
- python3 gpu_vllm_bench.py --config upstream-llama-8b.json  --config upstream-llama-3b.json    \
- --dashboard-csv prefill-decode-study-Llama-sweep-L4-maxmodellen-4096.csv \
+ python3 gpu_vllm_bench.py --config upstream-qwen3-4b-more.json     \
+ --dashboard-csv prefill-decode-study-Llama-sweep-L4-maxmodellen-32768.csv \
   --dashboard-accelerator "L4" \
   --mlflow \
   --output-base L4/Prefill-Decode-Sweep
